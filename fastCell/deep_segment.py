13fb9ae2675fefb421b3cdae03b932833cf67d90
nzxwang <nickywang100@gmail.com>
2019-06-28 16:44:08 -0400

    change deep_segment.py to only work on a single image

--- fastCell/deep_segment.py
+++ fastCell/deep_segment.py
@@ -7,11 +7,11 @@ import numpy as np
 from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Segment the cells from an image.')
-parser.add_argument("--images", dest="images", type=str, nargs="+", required=True,
-                    help = "Images to segment")
+parser.add_argument("--image", dest="image", type=str, required=True,
+                    help = "Image to segment")
 parser.add_argument("--learner", dest="learner", type=str, required=True,
                    help = "Load the Learner object that was saved from export().")
-parser.add_argument("--output-directory", dest="output_directory", type=str, required=True,)
+parser.add_argument("--segment-output", dest="segment_output", type=str, required=True,)
 parser.add_argument("--use-cuda", dest="use_cuda", action="store_true", default=False,
                    help = "Load the Learner object on the gpu instead of the cpu.")
 parser.add_argument("--crop-edges", dest="crop_edges", action="store_true", default=True,
@@ -25,8 +25,6 @@ args = parser.parse_args()
 l = 224
 
 if __name__ == '__main__':
-    output_dir = Path(args.output_directory)
-
     fastai.torch_core.defaults.device = torch.device('cuda') if args.use_cuda else torch.device('cpu')
     #this has to be done because of how stupidly learner() takes its arguments
     learner_path = Path(args.learner)
@@ -38,48 +36,45 @@ if __name__ == '__main__':
         temp_object = tempfile.TemporaryDirectory(dir=args.temp_dir)
         temp_dir = Path(temp_object.name)
 
-    for image_path in [Path(image) for image in args.images]:
-        if args.verbose:
-            print("Starting on " + image_path.as_posix())
-        image = cv.imread(image_path.as_posix(), cv.COLOR_BGR2GRAY)
+    image_path=Path(args.image)
+    image = cv.imread(args.image, cv.COLOR_BGR2GRAY)
 
-        if args.crop_edges:
+    if args.crop_edges:
+        if args.verbose:
+            print("Dimensions: " + str(image.shape[0]) + ", " + str(image.shape[1]))
+        image = image[image.shape[0] % l // 2:image.shape[0] - image.shape[0] % l // 2,
+                image.shape[1] % l // 2:image.shape[1] - image.shape[1] % l // 2]
+
+    i_max = image.shape[0] // l
+    j_max = image.shape[1] // l
+    if args.verbose:
+        print("Segmentation window is " + str(l) + "*" + str(l))
+        print("Image has " + str(i_max) + " rows and " + str(j_max) + " columns of image tiles.")
+
+    for i in range(i_max):
+        for j in range(j_max):
             if args.verbose:
-                print("Dimensions: " + str(image.shape[0]) + ", " + str(image.shape[1]))
-            image = image[image.shape[0] % l // 2:image.shape[0] - image.shape[0] % l // 2,
-                    image.shape[1] % l // 2:image.shape[1] - image.shape[1] % l // 2]
+                print("Working on tile in row:" + str(i+1) + ", column:" + str(j+1) + ".")
 
-        i_max = image.shape[0] // l
-        j_max = image.shape[1] // l
-        if args.verbose:
-            print("Segmentation window is " + str(l) + "*" + str(l))
-            print("Image has " + str(i_max) + " rows and " + str(j_max) + " columns of image tiles.")
+            # this is annoying, but a low-risk way to get fastai to read the image properly
+            image_tile = image[l * i:l * (i + 1), l * j:l * (j + 1)]
+            image_tile_path = temp_dir / (image_path.stem + "_i" + str(i) + "_j" + str(j) + image_path.suffix)
+            cv.imwrite(image_tile_path.as_posix(), image_tile)
+            image_tile = fastai.vision.open_image(image_tile_path.as_posix())
 
-        for i in range(i_max):
-            for j in range(j_max):
-                if args.verbose:
-                    print("Working on tile in row:" + str(i+1) + ", column:" + str(j+1) + ".")
+            segment_tile = learn.predict(image_tile)[0]._px.squeeze().numpy()
 
-                # this is annoying, but a low-risk way to get fastai to read the image properly
-                image_tile = image[l * i:l * (i + 1), l * j:l * (j + 1)]
-                image_tile_path = temp_dir / (image_path.stem + "_i" + str(i) + "_j" + str(j) + image_path.suffix)
-                cv.imwrite(image_tile_path.as_posix(), image_tile)
-                image_tile = fastai.vision.open_image(image_tile_path.as_posix())
+            if i==0 and j==0:
+                segment = segment_tile
+            elif i==0 and j!=0:
+                segment = np.concatenate((segment, segment_tile), axis=1)
+            elif i!=0 and j==0:
+                row = segment_tile
+            elif i!=0 and j!=0:
+                row = np.concatenate((row, segment_tile), axis=1)
 
-                segment_tile = learn.predict(image_tile)[0]._px.squeeze().numpy()
-
-                if i==0 and j==0:
-                    segment = segment_tile
-                elif i==0 and j!=0:
-                    segment = np.concatenate((segment, segment_tile), axis=1)
-                elif i!=0 and j==0:
-                    row = segment_tile
-                elif i!=0 and j!=0:
-                    row = np.concatenate((row, segment_tile), axis=1)
-
-            #After an entire row finishes
-            if i!=0:
-                segment = np.concatenate((segment, row), axis=0)
-        segment_path = output_dir/(image_path.stem + "_segment" + image_path.suffix)
-        segment[segment == 1] = 255
-        cv.imwrite(segment_path.as_posix(), segment)
+        #After an entire row finishes
+        if i!=0:
+            segment = np.concatenate((segment, row), axis=0)
+    segment[segment == 1] = 255
+    cv.imwrite(args.segment_output, segment)
