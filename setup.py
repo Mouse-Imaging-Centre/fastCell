c9de574f858419998d6143e2c93d84c2da4de9aa
nzxwang <nickywang100@gmail.com>
2019-11-04 11:14:26 -0500

    python>=3.6, fastai>=1.0.53 and description

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1",
     author="Nick Wang",
     author_email="nick.wang@mail.mcgill.ca",
-    description="Cell Finder",
+    description="Fast Deep Neural Networks for Cell Image Analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Mouse-Imaging-Centre/fastCell",
@@ -18,12 +18,14 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
     scripts=[
         'fastCell/deep_segment.py',
         'fastCell/pixelize_segment.py'
     ],
     install_requires=[
         'opencv-python==4.1,==3.4'
-        'pandas>=0.25'
+        'pandas>=0.25',
+        'fastai>=1.0.53'
     ]
 )
